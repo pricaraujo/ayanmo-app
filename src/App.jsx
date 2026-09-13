e11ee1cad8c2e94b874e87d6808a78ff2a60f0e2
pricaraujo <pri11araujo41@gmail.com>
2026-09-13 09:48:39 -0300

    tenta corrigir layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css' // Esta linha é obrigatória para injetar o layout
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
 
 // Brand Colors
 const COLORS = {
